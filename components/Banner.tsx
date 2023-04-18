4e6d39cb064febda043fb159b0e2f905aa0b8d64
Mallory Daniel <mllr.dnl@gmail.com>
2023-04-18 14:22:59 -0700

    design updates

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,7 @@ function Banner() {
     <div className={styles.bannerContainer}>
       <div className={styles.banner}>
         {/* <h1>Mal&apos;s Moderate Problem</h1> */}
-        <h1>MAL&apos;S MODERATE PROBLEM BLOG</h1>
+        <h1>MAL&apos;S MODERATE PROBLEM</h1>
         <h2>Welcome to my blog about navigating the sober life!</h2>
         <h4>SOBRIETY | MENTAL HEALTH | HABITS | WELLBEING</h4>
       </div>
